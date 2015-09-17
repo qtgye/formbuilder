97c837430b62aafe7f13aadb5655478fa5a96059
Jace <buquia.jace@gmail.com>
2015-09-18 00:47:42 +0800

    remove unnecessary scripts

--- js/partials/request.js
+++ js/partials/request.js
@@ -41,25 +41,7 @@ App.createModule('request',(function (app,$) {
 					}).join('&');
 			}
 		}
-	},
-
-	nativeRequest = (function () {
-		var request;
-		if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-		  request = new XMLHttpRequest();
-		} else if (window.ActiveXObject) { // IE
-		  try {
-		    request = new ActiveXObject('Msxml2.XMLHTTP');
-		  } 
-		  catch (e) {
-		    try {
-		      request = new ActiveXObject('Microsoft.XMLHTTP');
-		    } 
-		    catch (e) {}
-		  }
-		}
-		return request;
-	})();
+	};
 
 	// define private functions
 	// ====================================================================================
@@ -83,36 +65,6 @@ App.createModule('request',(function (app,$) {
 			error 		: errorCallback
 		});
 	}
-
-	// sends a request using native xmlHTTP request
-	// function nativeSend (data,successCallback,errorCallback) {
-	// 	var url = POST.url + ( data.id ? '/' + data.id : '');
-		
-
-	// 	nativeRequest.addEventListener("load", function (r) {
-	// 		var jsonData = JSON.parse(r.originalTarget.responseText);
-	// 		if ( jsonData.flag ) {
-	// 			successCallback(jsonData);
-	// 		} else {
-	// 			errorCallback(jsonData);
-	// 		}			
-	// 	});
-	// 	nativeRequest.addEventListener("error", function (response) {
-	// 		console.log(response);
-	// 	});
-
-	// 	// process data before sending
-	// 	var jsonString = JSON.stringify(data);
-	// 	// jsonString = jsonString.replace(/\"\d+\"/g,('$&').replace('\"',''));
-	// 	jsonString = jsonString.replace(/\"\d+\"/g,function (match) {
-	// 		return match.replace('"','','g');
-	// 	});
-	// 	console.log(jsonString);
-
-	// 	nativeRequest.open('POST', url, true);
-	// 	nativeRequest.setRequestHeader('Content-Type', 'application/json');
-	// 	nativeRequest.send(jsonString);
-	// }
 
 	// gets a list of latest forms
 	function get (successCallback,errorCallback) {
@@ -141,7 +93,6 @@ App.createModule('request',(function (app,$) {
 	// ====================================================================================
 
 	module.send 		= send;
-	// module.nativeSend 	= nativeSend;
 	module.get 			= get;
 	module.getForm 		= getForm;
 
